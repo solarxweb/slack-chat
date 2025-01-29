76a79f5261dbfc58bb0493db8404fc6de621eeeb
dev <legendaryje@gmail.com>
2025-01-29 22:35:03 +0300

    makefile change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,28 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        'react-redux',
-        'react',
-        'react-dom'
-      ]
-    }
-  },
   server: {
     port: 5002,
     proxy: {
+      // Проксируем запросы к API
       '/api': {
-        target: 'http://localhost:5001',
+        // target: 'http://localhost:5001',
+        target: 'http://127.0.0.1:5001',
         changeOrigin: false,
         secure: false,
       },
+      cors: false,
+      // Проксируем WebSocket соединения
       '/socket.io': {
-        target: 'ws://localhost:5001',
+        // target: 'ws://localhost:5001',
+        target: 'ws://127.0.0.1:5001',
         ws: true,
         rewriteWsOrigin: true,
       },
