8d4e880740a0b88d2bd516b4289222a5e09c0084
dev <legendaryje@gmail.com>
2025-01-30 23:20:21 +0300

    all edits after linter[5]

--- frontend/src/components/LoginForm/LoginForm.jsx
+++ frontend/src/components/LoginForm/LoginForm.jsx
@@ -1,19 +1,19 @@
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
-import { toast } from 'react-toastify';
-import './LoginForm.css';
-import { Spinner } from 'react-bootstrap';
-import { API_ROUTES } from '../../api';
-import * as yup from 'yup';
-import { useState } from 'react';
-import { setUserData } from '../../store/authSlice';
-import { useFormik } from 'formik';
-import axios from 'axios';
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { toast } from "react-toastify";
+import "./LoginForm.css";
+import { Spinner } from "react-bootstrap";
+import API_ROUTES from "../../api";
+import * as yup from "yup";
+import { useState } from "react";
+import { setUserData } from "../../store/authSlice";
+import { useFormik } from "formik";
+import axios from "axios";
 
 const LoginForm = () => {
   const { t } = useTranslation();
-  const noticeError = () => toast.warning(t('errNetwork'));
+  const noticeError = () => toast.warning(t("errNetwork"));
 
   const redir = useNavigate();
   const dispatch = useDispatch();
@@ -26,20 +26,20 @@ const LoginForm = () => {
 
   const formik = useFormik({
     initialValues: {
-      username: '',
-      password: '',
+      username: "",
+      password: "",
     },
     validationSchema,
     onSubmit: async (values) => {
       setAuthError(false);
       try {
         const { data: user } = await axios.post(API_ROUTES.login(), values);
-        localStorage.setItem('username', user.username);
-        localStorage.setItem('token', user.token);
+        localStorage.setItem("username", user.username);
+        localStorage.setItem("token", user.token);
         dispatch(setUserData(user));
-        redir('/');
+        redir("/");
       } catch (error) {
-        console.error('Login error:', error);
+        console.error("Login error:", error);
         if (error.response && error.response.status === 401) {
           setAuthError(true);
         } else if (error.response && error.response.status === 500) {
@@ -50,66 +50,66 @@ const LoginForm = () => {
   });
 
   return (
-    <div className='auth-wrapper'>
-      <form onSubmit={formik.handleSubmit} className='auth-form'>
-        <div className='auth-fields'>
-          <div className='auth-title'>
-            <h1>{t('enter')}</h1>
+    <div className="auth-wrapper">
+      <form onSubmit={formik.handleSubmit} className="auth-form">
+        <div className="auth-fields">
+          <div className="auth-title">
+            <h1>{t("enter")}</h1>
           </div>
-          <div className='form-floating mb-3'>
+          <div className="form-floating mb-3">
             {/* Поле имени пользователя */}
             <input
-              id='username'
-              placeholder={t('loginName')}
+              id="username"
+              placeholder={t("loginName")}
               required
-              className='input-field form-control'
-              type='text'
-              name='username'
+              className="input-field form-control"
+              type="text"
+              name="username"
               value={formik.values.username}
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
             />
-            <label className='label-form' htmlFor='username'>
-              {t('loginName')}
+            <label className="label-form" htmlFor="username">
+              {t("loginName")}
             </label>
           </div>
 
-          <div className='form-floating mb-3'>
+          <div className="form-floating mb-3">
             {/* Поле пароля */}
             <input
-              id='password'
-              placeholder={t('loginPassword')}
+              id="password"
+              placeholder={t("loginPassword")}
               required
-              className='input-field form-control'
-              type='password'
-              name='password'
+              className="input-field form-control"
+              type="password"
+              name="password"
               value={formik.values.password}
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
             />
-            <label className='label-form' htmlFor='password'>
-              {t('loginPassword')}
+            <label className="label-form" htmlFor="password">
+              {t("loginPassword")}
             </label>
-            {authError && <div className='error-message'>{t('errLogin')}</div>}
+            {authError && <div className="error-message">{t("errLogin")}</div>}
           </div>
           <button
-            type='submit'
-            className='submit-btn'
+            type="submit"
+            className="submit-btn"
             disabled={formik.isSubmitting}
           >
             {formik.isSubmitting ? (
-              <Spinner style={{ width: '15px', height: '15px' }} />
+              <Spinner style={{ width: "15px", height: "15px" }} />
             ) : (
-              t('enter')
+              t("enter")
             )}
           </button>
         </div>
 
-        <div className='auth-unregistered'>
+        <div className="auth-unregistered">
           <p>
-            {t('noAccount')}{' '}
-            <a className='auth-unregistered__signup' href='/signup'>
-              {t('signUpTitle')}
+            {t("noAccount")}{" "}
+            <a className="auth-unregistered__signup" href="/signup">
+              {t("signUpTitle")}
             </a>
           </p>
         </div>
